a0a7aa19b75bab727d23e4ecafcae1832b42b84f
Charanscode <charankumar6198@gmail.com>
2022-06-13 07:32:14 +0530

    first commit

---
+++ src/test/java/qaclickacademy/seliniumjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.seliniumjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeTestAndroid() {
+		System.out.println("NativeTestAndroid");
+	}
+
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/seliniumjava/RestapiTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy.seliniumjava;
+
+import org.testng.annotations.Test;
+
+public class RestapiTest {
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
